5f4669194d766607829be92a1ac48e53fcc728b2
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-22 01:18:38 +0300

    sculptureList

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { sculptureList } from "./data/sculptureList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let index = 0;
+  let sculpture = sculptureList[index];
 
+  function handlePreviousClick() {
+    console.log("Geri clicked");
+  }
+  function handleNextClick() {
+    console.log("İleri clicked");
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handlePreviousClick}>Geri</button>
+      <button onClick={handleNextClick}>İleri</button>
+
+      <h2>
+        <i>
+          {sculpture.name} by {sculpture.artist}
+        </i>
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt}></img>
+      <p>{sculpture.description}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
